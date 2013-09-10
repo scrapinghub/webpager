382dca7bfb1b46798e47f35d8ea37f5ef0d94e2c
tpeng <pengtaoo@gmail.com>
2013-09-10 16:45:33 +0800

    add main in train.py

--- train.py
+++ train.py
@@ -9,53 +9,54 @@ from os.path import join
 from webpager.features import HtmlFeaturesExtractor, AnchorTransformers
 
 
-htmlFeatGen = HtmlFeaturesExtractor()
-anchorFeatGen = FeatureUnion(AnchorTransformers)
-
 def tagged_data(folder):
     for fname in os.listdir(folder):
         path = os.path.join(folder, fname)
         with open(path, 'r') as f:
             yield path, f.read()
 
-anchors = []
-labels = []
+if __name__ == '__main__':
+    htmlFeatGen = HtmlFeaturesExtractor()
+    anchorFeatGen = FeatureUnion(AnchorTransformers)
 
-for _, html in tagged_data('corpus/annotated'):
-    anchor, label = htmlFeatGen.fit_transform(html, encoding='utf8')
-    anchors.extend(anchor)
-    labels.extend(label)
+    anchors = []
+    labels = []
 
-documents = anchorFeatGen.fit_transform(anchors[:3000])
-test_documents = anchorFeatGen.transform(anchors[3000:])
+    for _, html in tagged_data('corpus/annotated'):
+        anchor, label = htmlFeatGen.fit_transform(html, encoding='utf8')
+        anchors.extend(anchor)
+        labels.extend(label)
 
-baseclf = DummyClassifier(strategy='most_frequent',random_state=0)
-baseclf.fit(documents, labels[:3000])
+    documents = anchorFeatGen.fit_transform(anchors[:3000])
+    test_documents = anchorFeatGen.transform(anchors[3000:])
 
-clf = LogisticRegression(tol=1e-8, penalty='l2', C=7, class_weight='auto')
-clf.fit(documents, labels[:3000])
+    baseclf = DummyClassifier(strategy='most_frequent',random_state=0)
+    baseclf.fit(documents, labels[:3000])
 
-predicted = clf.predict(test_documents)
-print confusion_matrix(labels[3000:], predicted)
-print 'accuracy', accuracy_score(labels[3000:], predicted)
-print 'recall', recall_score(labels[3000:], predicted)
+    clf = LogisticRegression(tol=1e-8, penalty='l2', C=7, class_weight='auto')
+    clf.fit(documents, labels[:3000])
 
-print '-' * 80
+    predicted = clf.predict(test_documents)
+    print confusion_matrix(labels[3000:], predicted)
+    print 'accuracy', accuracy_score(labels[3000:], predicted)
+    print 'recall', recall_score(labels[3000:], predicted)
 
-base_predicted = baseclf.predict(test_documents)
-print confusion_matrix(labels[3000:], base_predicted)
-print 'accuracy', accuracy_score(labels[3000:], base_predicted)
-print 'recall', recall_score(labels[3000:], base_predicted)
+    print '-' * 80
 
-def print_topN(vectorizer, clf, n=20):
-    """Prints features with the highest coefficient values, per class"""
-    feature_names = vectorizer.get_feature_names()
-    top10 = np.argsort(clf.coef_[0])[-n:]
-    print(" ".join(feature_names[j] for j in top10))
+    base_predicted = baseclf.predict(test_documents)
+    print confusion_matrix(labels[3000:], base_predicted)
+    print 'accuracy', accuracy_score(labels[3000:], base_predicted)
+    print 'recall', recall_score(labels[3000:], base_predicted)
 
-print '-' * 80
-print_topN(anchorFeatGen, clf)
+    def print_topN(vectorizer, clf, n=20):
+        """Prints features with the highest coefficient values, per class"""
+        feature_names = vectorizer.get_feature_names()
+        top10 = np.argsort(clf.coef_[0])[-n:]
+        print(" ".join(feature_names[j] for j in top10))
 
-joblib.dump(htmlFeatGen, join('webpager', 'models', 'htmlFeatGen.joblib.pkl'), compress=9)
-joblib.dump(anchorFeatGen, join('webpager', 'models', 'anchorFeatGen.joblib.pkl'), compress=9)
-joblib.dump(clf, join('webpager', 'models', 'clf.joblib.pkl'), compress=9)
+    print '-' * 80
+    print_topN(anchorFeatGen, clf)
+
+    joblib.dump(htmlFeatGen, join('webpager', 'models', 'htmlFeatGen.joblib.pkl'), compress=9)
+    joblib.dump(anchorFeatGen, join('webpager', 'models', 'anchorFeatGen.joblib.pkl'), compress=9)
+    joblib.dump(clf, join('webpager', 'models', 'clf.joblib.pkl'), compress=9)
