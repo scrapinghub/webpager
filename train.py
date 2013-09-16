a1c06624077e10fb1272e78a59d8aa943fee1e43
tpeng <pengtaoo@gmail.com>
2013-09-16 14:59:15 +0800

    use sklearn.crossvalidation.train_test_split to simplify the train process

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 import os
 from sklearn.dummy import DummyClassifier
 from sklearn.externals import joblib
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, recall_score
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, recall_score, precision_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import FeatureUnion
+from sklearn.cross_validation import train_test_split
 import numpy as np
 from os.path import join
 from webpager.features import HtmlFeaturesExtractor, AnchorTransformers
@@ -27,35 +28,29 @@ if __name__ == '__main__':
         anchors.extend(anchor)
         labels.extend(label)
 
-    documents = anchorFeatGen.fit_transform(anchors[:3000])
-    test_documents = anchorFeatGen.transform(anchors[3000:])
-
-    baseclf = DummyClassifier(strategy='most_frequent',random_state=0)
-    baseclf.fit(documents, labels[:3000])
+    train_anchors, test_anchors, train_labels, test_labels = train_test_split(anchors, labels, test_size=0.25, random_state=42)
+    train_documents = anchorFeatGen.fit_transform(train_anchors)
+    test_documents = anchorFeatGen.transform(test_anchors)
 
     clf = LogisticRegression(tol=1e-8, penalty='l2', C=7, class_weight='auto')
-    clf.fit(documents, labels[:3000])
+    clf.fit(train_documents, train_labels)
 
     predicted = clf.predict(test_documents)
-    print confusion_matrix(labels[3000:], predicted)
-    print 'accuracy', accuracy_score(labels[3000:], predicted)
-    print 'recall', recall_score(labels[3000:], predicted)
+    print '# train documents', train_documents.shape[0]
+    print '# test documents', test_documents.shape[0]
+    print 'confusion matrix:'
+    print confusion_matrix(test_labels, predicted)
+    print 'accuracy', accuracy_score(test_labels, predicted)
+    print 'precision', precision_score(test_labels, predicted)
+    print 'recall', recall_score(test_labels, predicted)
 
-    print '-' * 80
+    def show_most_informative_features(vectorizer, clf, n=20):
+        c_f = sorted(zip(clf.coef_[0], vectorizer.get_feature_names()))
+        top = zip(c_f[:n], c_f[:-(n+1):-1])
+        for (c1,f1),(c2,f2) in top:
+            print "\t%.4f\t%-15s\t\t%.4f\t%-15s" % (c1,f1,c2,f2)
 
-    base_predicted = baseclf.predict(test_documents)
-    print confusion_matrix(labels[3000:], base_predicted)
-    print 'accuracy', accuracy_score(labels[3000:], base_predicted)
-    print 'recall', recall_score(labels[3000:], base_predicted)
-
-    def print_topN(vectorizer, clf, n=20):
-        """Prints features with the highest coefficient values, per class"""
-        feature_names = vectorizer.get_feature_names()
-        top10 = np.argsort(clf.coef_[0])[-n:]
-        print(" ".join(feature_names[j] for j in top10))
-
-    print '-' * 80
-    print_topN(anchorFeatGen, clf)
+    show_most_informative_features(anchorFeatGen, clf)
 
     joblib.dump(htmlFeatGen, join('webpager', 'models', 'htmlFeatGen.joblib.pkl'), compress=9)
     joblib.dump(anchorFeatGen, join('webpager', 'models', 'anchorFeatGen.joblib.pkl'), compress=9)
